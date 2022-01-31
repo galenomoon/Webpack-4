b912a144a26a7537bed8bbd1f8c331c88a6a5ece
galeno <gui.galenocover@gmail.com>
2022-01-31 15:57:42 -0300

    Development and Production Mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
+const modoDev = process.env.NODE_ENV !== "production";
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: modoDev ? "development" : "production",
   entry: "./src/principal.js",
   output: {
     filename: "principal.js",
     path: __dirname + "./public",
   },
-  plugins:[
-      new MiniCssExtractPlugin({
-          filename: estilo.css
-      })
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptimizeCssAssetsPlugin({}),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: estilo.css,
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.s?[ac]ss$/,
         use: [
-            MiniCssExtractPlugin.loader,
-            "style-loader", 
-            "css-loader",
-            "sass-loader",
+          MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "sass-loader",
         ],
       },
     ],
